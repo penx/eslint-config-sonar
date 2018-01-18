dd3ef4684c87252dc78d2d4170d7a39a7aa0fc16
Alasdair McLeay <alasdair.mcleay@me.com>
2018-01-18 15:04:57 +0000

    fix lint rules

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 module.exports = {
   rules: {
-    'no-alert': [2, "always"],
-    'no-eval': [2, "always"],
-    'no-wildcard-postmessage': [2, "always"],
-    'no-debugger': [2, "always"],
-    'no-new-func': [2, "always"]
-  }
+    'no-alert': 2,
+    'no-eval': 2,
+    'no-debugger': 2,
+    'no-new-func': 2,
+    'no-wildcard-postmessage/no-wildcard-postmessage': 2
+  },
+  plugins: ['no-wildcard-postmessage']
 };
