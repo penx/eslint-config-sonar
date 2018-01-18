32d944ffa8a9439f1ae0bcc391828596023b1ac9
Alasdair McLeay <alasdair.mcleay@me.com>
2018-01-18 14:36:10 +0000

    initial lint rules

---
+++ index.js
@@ -0,0 +1,9 @@
+module.exports = {
+  rules: {
+    'no-alert': [2, "always"],
+    'no-eval': [2, "always"],
+    'no-wildcard-postmessage': [2, "always"],
+    'no-debugger': [2, "always"],
+    'no-new-func': [2, "always"]
+  }
+};
