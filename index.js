4035557e73a4797677f11ab981b73ec060fdc15f
Alasdair McLeay <alasdair.mcleay@me.com>
2018-01-19 14:53:55 +0000

    set no-alert and no-debugger to warn rather than error to match sonar minor severity

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 module.exports = {
   rules: {
-    'no-alert': 2,
+    // Vulnerabilities: https://sonarcloud.io/organizations/opensource/rules#qprofile=AV7g2D3v6oOvObTbiZD9|activation=true|types=VULNERABILITY
+    'no-alert': 1,
     'no-eval': 2,
-    'no-debugger': 2,
+    'no-debugger': 1,
     'no-new-func': 2,
     'no-wildcard-postmessage/no-wildcard-postmessage': 2
   },
